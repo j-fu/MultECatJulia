87b6aac9b1040e8327b8562870e4b4e729a00e5e
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2021-11-15 18:58:26 +0100

    increase default gridgaller resolution

--- scripts/gridgallery.jl
+++ scripts/gridgallery.jl
@@ -10,7 +10,7 @@ using MultECatJulia
 Show a gallery of grids
 """
 function main()
-    vis=GridVisualizer(Plotter=GLMakie,resolution=(1200,400), layout=(1,3))
+    vis=GridVisualizer(Plotter=GLMakie,resolution=(1500,500), layout=(1,3))
     gridplot!(vis[1,1],foursquare_electrode_grid(), xplanes=[0.0],zplanes=[0.3],outlinealpha=0.3)
     gridplot!(vis[1,2],circular_electrode_grid(), xplanes=[0.0],zplanes=[0.3],outlinealpha=0.3)
     gridplot!(vis[1,3],circular_anisoref_electrode_grid(), xplanes=[0.0],zplanes=[0.3],outlinealpha=0.3)
